ae1d257d0ac06acd00e54fb713a06b62f337abc7
Sa888nO <shura.reno.97@bk.ru>
2022-01-17 00:09:55 +0300

    making a backend

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.use(
+  express.static(
+    path.resolve(__dirname, "projectSources/static/chooseRole/choose-role")
+  )
+);
+
+app.get("*", (req, res) => {
+  res.sendFile(
+    path.resolve(
+      __dirname,
+      "projectSources/static/chooseRole/choose-role",
+      "main.html"
+    )
+  );
+});
+
+app.listen(3000, () => console.log("Server has been started on port 3000"));
